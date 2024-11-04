b0bcef58499395d6057076adc2eb0944001c2281
Oskar <o.pulczynski@gmail.com>
2024-11-04 15:45:40 +0100

    Markup for sections: header, about and skills

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+      fontFamily: {
+				dmsans: ["DM Sans", "sans-serif"],
+			},
+			colors: {
+				bg: "#020916",
+			},
+		},
+	},
+	plugins: [],
+};
