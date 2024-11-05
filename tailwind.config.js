0c4e76f76f097954e5f10be7d51aa31f84475a7b
Oskar <o.pulczynski@gmail.com>
2024-11-05 08:00:57 +0100

    Added mobile styles for skills section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,22 @@ module.exports = {
 	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
-      fontFamily: {
+			fontFamily: {
 				dmsans: ["DM Sans", "sans-serif"],
 			},
 			colors: {
 				bg: "#020916",
 			},
 			maxWidth: {
-				'2xs': '20,4375rem',
-        '8xl': '82rem',
-      },
+				"2xs": "20.4375rem",
+				"8xl": "82rem",
+			},
+			borderRadius: {
+				ten: "0.625rem",
+			},
+			boxShadow: {
+				'innerlight': 'inset 0 2px 0 0 rgba(255,255,255,0.15)'
+			},
 		},
 	},
 	plugins: [],
